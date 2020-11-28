f14401857faf3199b352fcb99d1b5a8b529e0440
Sergey <50179609+ED4M@users.noreply.github.com>
2020-11-28 13:52:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import cx_Oracle
+import os
+import config
+from pprint import pprint
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo=False)
+
+# cp1251
+df = pd.read_csv("import.csv",sep=';',encoding="cp1251") 
+
+
+mapped = df[(df['ADDRESS'] != None) & (df['ADDRESS'].str.contains("Москва"))]
+mapped = mapped.replace(np.nan, '', regex=True)
+
+writer = pd.ExcelWriter('export.xlsx', engine='xlsxwriter')
+mapped.to_excel(writer, 'Sheet1')
+writer.save()
+
+# Oracle
+LOCATION_ORACLE = r"D:\Prog\instantclient_19_9"
+os.environ["PATH"] = LOCATION_ORACLE + ";" + os.environ["PATH"]
+
+conn = cx_Oracle.connect(
+    config.user, 
+    config.password, 
+    config.dsn, 
+    encoding=config.encoding)
+curs = conn.cursor()
+
+for item in mapped.values:
+  curs.execute(
+    "INSERT INTO FNS_BASE (KOD, NAIMK, NAIM_FULL, ADDRESS, COMMENTS) VALUES (:KOD, :NAIMK, :NAIM_FULL, :ADDRESS, :COMMENTS)",
+      KOD=item[0], NAIMK=item[1], NAIM_FULL=item[2], ADDRESS=item[3], COMMENTS=item[4]
+      )
+
+conn.commit()
+print('Done importing!')
+conn.close()
